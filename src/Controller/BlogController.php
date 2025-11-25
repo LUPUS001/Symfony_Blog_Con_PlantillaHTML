0d9801dc20cbf2b53b802bb784508c835331c9bc
LUPUS001 <antoniodaw23@gmail.com>
2025-11-25 09:16:10 +0000

    Primer commit - Hasta el 1.2.3

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'blog')]
+    public function blog(): Response
+    {
+        return $this->render('blog/blog.html.twig', []);
+    }
+
+    #[Route('/single_post', name: 'single_post')]
+    public function single_post(): Response
+    {
+        return $this->render('blog/single_post.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+}
