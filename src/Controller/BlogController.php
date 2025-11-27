08ec094e66c6c661e913a3fc6a592b1861cd9bf0
LUPUS001 <antoniodaw23@gmail.com>
2025-11-27 08:03:26 +0000

    Tercer Commit - Reto 1.3.1 FINALIZADO

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/blog')]
 final class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'blog')]
+    #[Route('', name: 'blog')]
     public function blog(): Response
     {
         return $this->render('blog/blog.html.twig', []);
@@ -21,4 +22,18 @@ final class BlogController extends AbstractController
             'controller_name' => 'BlogController',
         ]);
     }
+
+    //Por si queremos hacer la ruta del post único dinámica:
+    /*
+    #[Route('/single-post/{slug}', name: 'single_post')]
+    public function single_post(string $slug): Response
+    {
+        // En un proyecto real, usarías $slug para buscar el post en la base de datos.
+        // Aquí lo pasamos a la plantilla para que sepa qué post está cargando.
+        return $this->render('blog/single_post.html.twig', [
+            'slug' => $slug,
+        ]);
+    }
+    */
+
 }
